a7095fdb7c93744fc09f832f6eadb5d26f3d9e52
meganpierce1 <nicomegan@gmail.com>
2024-12-06 17:52:45 -0700

    Navbar done. Could use some cleanup and maybe a second look at the way I did this but I want to move on for now.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function toggleDropdown(event) {
     let navbarToggle = document.getElementById("navbar-toggle");
     event.preventDefault();
-    navbarToggle.classList.toggle("responsive");
+    navbarToggle.classList.toggle("open");
 }
