fcc762027b05e06d69d7596180a5bb47c4e17dec
meganpierce1 <nicomegan@gmail.com>
2024-12-31 15:35:21 -0700

    Finished contact form

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
+const form = document.getElementById('form');
+
 function toggleDropdown(event) {
     let navbarToggle = document.getElementById("navbar-toggle");
     event.preventDefault();
     navbarToggle.classList.toggle("open");
+}
+
+function showForm() {
+    form.style.display = 'block';
+    document.body.classList.add('no-scroll'); //prevent scrolling
+}
+
+function hideForm() {
+    form.style.display = 'none'; // Hide the form
+    document.body.classList.remove('no-scroll'); // Re-enable scrolling
+}
+
+function submitForm() {
+    hideForm(); // make this call for now until adding actual submit functionality
 }
